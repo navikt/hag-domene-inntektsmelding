8b99e4c77952613a934d9a73c49de3442b68f32b
Mikael Bjerga <mikael.bjerga@nav.no>
2023-10-03 14:04:17 +0200

    La begrunnelse-enum matche kodeverkverdier (UpperCamelCase)

--- src/main/kotlin/no/nav/helsearbeidsgiver/domene/inntektsmelding/FullLoennIArbeidsgiverPerioden.kt
+++ src/main/kotlin/no/nav/helsearbeidsgiver/domene/inntektsmelding/FullLoennIArbeidsgiverPerioden.kt
@@ -1,8 +1,6 @@
 package no.nav.helsearbeidsgiver.domene.inntektsmelding
 
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.SerializationException
-import no.nav.helsearbeidsgiver.utils.json.serializer.AsStringSerializer
 
 @Serializable
 data class FullLoennIArbeidsgiverPerioden(
@@ -11,37 +9,22 @@ data class FullLoennIArbeidsgiverPerioden(
     val utbetalt: Double? = null,
 )
 
-@Serializable(BegrunnelseIngenEllerRedusertUtbetalingKodeSerializer::class)
-enum class BegrunnelseIngenEllerRedusertUtbetalingKode(val value: String) {
-    ARBEID_OPPHOERT("ArbeidOpphoert"),
-    BESKJED_GITT_FOR_SENT("BeskjedGittForSent"),
-    BETVILER_ARBEIDSUFOERHET("BetvilerArbeidsufoerhet"),
-    FERIE_ELLER_AVSPASERING("FerieEllerAvspasering"),
-    FISKER_MED_HYRE("FiskerMedHyre"),
-    FRAVAER_UTEN_GYLDIG_GRUNN("FravaerUtenGyldigGrunn"),
-    IKKE_FRAVAER("IkkeFravaer"),
-    IKKE_FULL_STILLINGSANDEL("IkkeFullStillingsandel"),
-    IKKE_LOENN("IkkeLoenn"),
-    LOVLIG_FRAVAER("LovligFravaer"),
-    MANGLER_OPPTJENING("ManglerOpptjening"),
-    PERMITTERING("Permittering"),
-    SAERREGLER("Saerregler"),
-    STREIK_ELLER_LOCKOUT("StreikEllerLockout"),
-    TIDLIGERE_VIRKSOMHET("TidligereVirksomhet"),
+/** Bruker UpperCamelCase for å matche kodeverkverdier.  */
+@Serializable
+enum class BegrunnelseIngenEllerRedusertUtbetalingKode {
+    ArbeidOpphoert,
+    BeskjedGittForSent,
+    BetvilerArbeidsufoerhet,
+    FerieEllerAvspasering,
+    FiskerMedHyre,
+    FravaerUtenGyldigGrunn,
+    IkkeFravaer,
+    IkkeFullStillingsandel,
+    IkkeLoenn,
+    LovligFravaer,
+    ManglerOpptjening,
+    Permittering,
+    Saerregler,
+    StreikEllerLockout,
+    TidligereVirksomhet,
 }
-
-internal class BegrunnelseIngenEllerRedusertUtbetalingKodeSerializer : AsStringSerializer<BegrunnelseIngenEllerRedusertUtbetalingKode>(
-    serialName = "helsearbeidsgiver.kotlinx.BegrunnelseIngenEllerRedusertUtbetalingKodeSerializer",
-    parse = { json ->
-        BegrunnelseIngenEllerRedusertUtbetalingKode.entries.firstOrNull { begrunnelse ->
-            listOf(
-                begrunnelse.name,
-                begrunnelse.value,
-            )
-                .any {
-                    it.equals(json, ignoreCase = true)
-                }
-        }
-            ?: throw SerializationException("Fant ingen begrunnelse med 'name' eller 'value' som matchet '$json'.")
-    },
-)
