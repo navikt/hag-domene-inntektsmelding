155bc4895845690ad1c1cfa28d66a175b20df94d
Mikael Bjerga <6940327+bjerga@users.noreply.github.com>
2024-05-02 14:06:23 +0200

    Fiks bestemmende fraværsdag (og ikke bruk egenmeldinger) (#21)
    * Fiks bestemmende fraværsdag (og ikke bruk egenmeldinger)
    
    * Fiks kall til bestemmende fraværsdager
    
    * Legg til test for spesielt tilfelle
    
    * Legg til konstanter

--- src/main/kotlin/no/nav/helsearbeidsgiver/domene/inntektsmelding/v1/BestemmendeFravaersdag.kt
+++ src/main/kotlin/no/nav/helsearbeidsgiver/domene/inntektsmelding/v1/BestemmendeFravaersdag.kt
@@ -1,55 +1,50 @@
 package no.nav.helsearbeidsgiver.domene.inntektsmelding.v1
 
-import java.time.DayOfWeek
+import no.nav.helsearbeidsgiver.domene.inntektsmelding.v1.utils.agpPaavirkerIkkeInntektsmelding
+import no.nav.helsearbeidsgiver.domene.inntektsmelding.v1.utils.daysUntil
+import no.nav.helsearbeidsgiver.domene.inntektsmelding.v1.utils.slaaSammenSammenhengendePerioder
 import java.time.LocalDate
-import java.time.temporal.ChronoUnit
+
+private const val AGP_MAKS_DAGER = 16
 
 fun bestemmendeFravaersdag(
     arbeidsgiverperioder: List<Periode>,
-    egenmeldingsperioder: List<Periode>,
     sykmeldingsperioder: List<Periode>,
 ): LocalDate {
-    val sisteArbeidsgiverperiode = arbeidsgiverperioder
-        .slaaSammenSammenhengendePerioder { denne, neste ->
-            denne.tom.daysUntil(neste.fom) <= 1
-        }
-        .lastOrNull()
+    val agpSlutt = arbeidsgiverperioder.lastOrNull()?.tom
+    val sykmeldingsperioderStart = sykmeldingsperioder.first().fom
 
-    val sisteSykdomsperiode = (egenmeldingsperioder + sykmeldingsperioder)
-        .slaaSammenSammenhengendePerioder(
-            kanSlaasSammen = ::kanSlaasSammenIgnorerHelgegap,
-        )
-        .fjernPerioderEtterFoersteUtoverAgp()
-        .last()
+    val sammenhengendeSykmeldingsperioder = sykmeldingsperioder.slaaSammenSammenhengendePerioder(ignorerHelgegap = true)
 
-    return if (sisteArbeidsgiverperiode != null) {
-        maxOf(
-            sisteArbeidsgiverperiode.fom,
-            sisteSykdomsperiode.fom,
-        )
+    val sammenhengendeFravaersperioder = if (
+        agpSlutt == null ||
+        agpPaavirkerIkkeInntektsmelding(agpSlutt, sykmeldingsperioderStart)
+    ) {
+        sammenhengendeSykmeldingsperioder
     } else {
-        sisteSykdomsperiode.fom
+        val sammenhengendeSykmeldingsperioderUtenAgp = sammenhengendeSykmeldingsperioder.fjernDatoerTilOgMed(agpSlutt)
+
+        (arbeidsgiverperioder + sammenhengendeSykmeldingsperioderUtenAgp)
+            .slaaSammenSammenhengendePerioder(ignorerHelgegap = false)
+            .fjernPerioderEtterFoersteUtoverAgp()
     }
+
+    return sammenhengendeFravaersperioder.last().fom
 }
 
-private fun List<Periode>.slaaSammenSammenhengendePerioder(
-    kanSlaasSammen: (Periode, Periode) -> Boolean,
-): List<Periode> =
-    sortedBy { it.fom }
-        .fold(emptyList()) { slaattSammen, periode ->
-            val forrige = slaattSammen.lastOrNull()
-
-            if (forrige != null && kanSlaasSammen(forrige, periode)) {
-                val sammenhengende = Periode(
-                    fom = forrige.fom,
-                    tom = maxOf(forrige.tom, periode.tom),
-                )
-
-                slaattSammen.dropLast(1).plus(sammenhengende)
-            } else {
-                slaattSammen.plus(periode)
-            }
+private fun List<Periode>.fjernDatoerTilOgMed(grenseTom: LocalDate): List<Periode> =
+    mapNotNull {
+        if (it.tom.isAfter(grenseTom)) {
+            it.copy(
+                fom = maxOf(
+                    it.fom,
+                    grenseTom.plusDays(1),
+                ),
+            )
+        } else {
+            null
         }
+    }
 
 private fun List<Periode>.fjernPerioderEtterFoersteUtoverAgp(): List<Periode> =
     filterIndexed { index, _ ->
@@ -58,17 +53,5 @@ private fun List<Periode>.fjernPerioderEtterFoersteUtoverAgp(): List<Periode> =
                 it.fom.daysUntil(it.tom) + 1
             }
 
-        antallForegaaendeDager <= 16
+        antallForegaaendeDager <= AGP_MAKS_DAGER
     }
-
-private fun kanSlaasSammenIgnorerHelgegap(denne: Periode, neste: Periode): Boolean {
-    val dagerAvstand = denne.tom.daysUntil(neste.fom)
-    return when (denne.tom.dayOfWeek) {
-        DayOfWeek.FRIDAY -> dagerAvstand <= 3
-        DayOfWeek.SATURDAY -> dagerAvstand <= 2
-        else -> dagerAvstand <= 1
-    }
-}
-
-private fun LocalDate.daysUntil(other: LocalDate): Int =
-    until(other, ChronoUnit.DAYS).toInt()
