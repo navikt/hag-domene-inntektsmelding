255c67604f2d5eada087b5d7811448321df14e30
Morten Byhring <morten.byhring@nav.no>
2024-05-15 10:30:11 +0200

    legg til type i gammel JournalførtInntektsmelding for å kunne skille på selvbestemt og vanlig i spino

--- src/main/kotlin/no/nav/helsearbeidsgiver/domene/inntektsmelding/deprecated/JournalfoertInntektsmelding.kt
+++ src/main/kotlin/no/nav/helsearbeidsgiver/domene/inntektsmelding/deprecated/JournalfoertInntektsmelding.kt
@@ -3,6 +3,7 @@ package no.nav.helsearbeidsgiver.domene.inntektsmelding.deprecated
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.JsonNames
+import no.nav.helsearbeidsgiver.domene.inntektsmelding.v1.Inntektsmelding.Type as Type
 
 @Deprecated("Bruk 'v1.JournalfoertInntektsmelding' istedenfor.")
 @Serializable
@@ -11,4 +12,5 @@ data class JournalfoertInntektsmelding(
     val journalpostId: String,
     @JsonNames("inntektsmeldingDokument") // TODO slett etter overgangsfase
     val inntektsmelding: Inntektsmelding,
+    val type: Type?, // for å skille på selvbestemt og vanlig i spinosaurus, før V1 tas i bruk overalt
 )
