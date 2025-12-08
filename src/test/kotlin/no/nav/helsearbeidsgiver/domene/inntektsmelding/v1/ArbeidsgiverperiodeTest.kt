c096791101fc36b5b60e064c45278afab76207a0
Mikael Bjerga <mikael.bjerga@nav.no>
2025-12-08 15:02:02 +0100

    Legg til flere tester

--- src/test/kotlin/no/nav/helsearbeidsgiver/domene/inntektsmelding/v1/ArbeidsgiverperiodeTest.kt
+++ src/test/kotlin/no/nav/helsearbeidsgiver/domene/inntektsmelding/v1/ArbeidsgiverperiodeTest.kt
@@ -1,8 +1,10 @@
 package no.nav.helsearbeidsgiver.domene.inntektsmelding.v1
 
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.datatest.withData
 import io.kotest.matchers.booleans.shouldBeFalse
 import io.kotest.matchers.booleans.shouldBeTrue
+import no.nav.helsearbeidsgiver.utils.test.date.oktober
 import no.nav.helsearbeidsgiver.utils.test.date.september
 
 class ArbeidsgiverperiodeTest :
@@ -23,16 +25,27 @@ class ArbeidsgiverperiodeTest :
                 tomAgp.erGyldigHvisIkkeForespurt(false, sykmeldingsperioder).shouldBeTrue()
             }
 
-            test("gir 'true' dersom AGP starter etter sykmeldingsperioden") {
+            withData(
+                nameFn = { (tilfelle, _) -> "gir 'true' dersom AGP starter $tilfelle" },
+                listOf(
+                    "på andre dag i sykmeldingsperioden" to mockAgp(7.september til 22.september),
+                    "på siste dag i sykmeldingsperioden" to mockAgp(30.september til 15.oktober),
+                ),
+            ) { (_, agpMedGyldigeDatoer) ->
                 val sykmeldingsperioder = listOf(6.september til 30.september)
-                val agpMedGyldigeDatoer = mockAgp(7.september til 22.september)
 
                 agpMedGyldigeDatoer.erGyldigHvisIkkeForespurt(false, sykmeldingsperioder).shouldBeTrue()
             }
 
-            test("gir 'false' dersom AGP starter samtidig som sykmeldingsperioden") {
+            withData(
+                nameFn = { (tilfelle, _) -> "gir 'false' dersom AGP starter $tilfelle" },
+                listOf(
+                    "før sykmeldingsperioden starter" to mockAgp(4.september til 19.september),
+                    "på samme dag som sykmeldingsperioden" to mockAgp(5.september til 20.september),
+                    "etter sykmeldingsperioden slutter" to mockAgp(1.oktober til 16.oktober),
+                ),
+            ) { (_, agpMedUgyldigeDatoer) ->
                 val sykmeldingsperioder = listOf(5.september til 30.september)
-                val agpMedUgyldigeDatoer = mockAgp(5.september til 20.september)
 
                 agpMedUgyldigeDatoer.erGyldigHvisIkkeForespurt(false, sykmeldingsperioder).shouldBeFalse()
             }
