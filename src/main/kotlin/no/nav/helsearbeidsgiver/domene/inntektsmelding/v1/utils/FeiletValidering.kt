6d6cab00bffeaf04839a5351300e469aa8464ba5
mortenbyhring <117746154+mortenbyhring@users.noreply.github.com>
2026-05-13 15:12:24 +0200

    Update src/main/kotlin/no/nav/helsearbeidsgiver/domene/inntektsmelding/v1/utils/FeiletValidering.kt
    Co-authored-by: Mikael Bjerga <6940327+bjerga@users.noreply.github.com>

--- src/main/kotlin/no/nav/helsearbeidsgiver/domene/inntektsmelding/v1/utils/FeiletValidering.kt
+++ src/main/kotlin/no/nav/helsearbeidsgiver/domene/inntektsmelding/v1/utils/FeiletValidering.kt
@@ -28,7 +28,7 @@ internal object Feilmelding {
     const val DUPLIKAT_INNTEKT_ENDRINGSAARSAK = "Endringsårsaker kan ikke inneholde duplikater"
 
     const val UGYLDIG_FLERE_ARBEIDSFORHOLD_MAA_HA_MINST_TO = "Flere arbeidsforhold må inneholde minst to arbeidsforhold"
-    const val UGYLDIG_FLERE_ARBEIDSFORHOLD_INGEN_ARBEIDSFORHOLD = "Minst ett arbeidsforhold skal være inkludert i sykepengegrunnlag"
+    const val UGYLDIG_FLERE_ARBEIDSFORHOLD_INGEN_ARBEIDSFORHOLD = "Minst ett arbeidsforhold må være inkludert i sykepengegrunnlaget"
     const val UGYLDIG_FLERE_ARBEIDSFORHOLD_ALLE_ARBEIDSFORHOLD = "Kan ikke inkludere alle arbeidsforhold, bruk vanlig inntektsmelding"
     const val UGYLDIG_FLERE_ARBEIDSFORHOLD_MED_LIK_LOENN = "Ved innsending av flere arbeidsforhold må de ha ulik lønn"
     const val UGYLDIG_FLERE_ARBEIDSFORHOLD_SYK_FRA_ALLE = "Ved innsending av flere arbeidsforhold kan det ikke være sykefravær fra alle"
